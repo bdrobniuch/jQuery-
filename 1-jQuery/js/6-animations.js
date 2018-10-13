e05b5743139dbacb294eb0e50133787bb8732003
bdrobniuch <drobniuch@gmail.com>
2018-10-13 14:16:13 +0200

    slider

--- 1-jQuery/js/6-animations.js
+++ 1-jQuery/js/6-animations.js
@@ -1,4 +1,10 @@
 $(function(){
    var mainHeading = $("#main-heading");
-mainHeading.hide();
+//mainHeading.hide(5000);
+//    mainHeading.show(5000);
+//    mainHeading.fadeIn(2000);
+//    mainHeading.slideUp(5000).slideDown(3000);
+    mainHeading.animate({fontSize: "10em"}, 1000, function() {
+        document.location.href=("http://google.com");
+    });
 });
