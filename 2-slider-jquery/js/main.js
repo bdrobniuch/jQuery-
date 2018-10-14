21c08b0c5eab0a654019d1e9c9217d146684ccf1
bdrobniuch <drobniuch@gmail.com>
2018-10-14 11:07:41 +0200

    poprawka efektu

--- 2-slider-jquery/js/main.js
+++ 2-slider-jquery/js/main.js
@@ -63,17 +63,39 @@ $(function () {
     function update() {
         enable = false;
         $(".slide-caption").eq(slideIndex).fadeOut(0);
+
+        if (slideIndex == slideCount - 1) {
+            $(".slide-caption").eq(1).fadeOut(0);
+            console.log(slideIndex);
+        }
+        if (slideIndex == 0) {
+            $(".slide-caption").eq(slideCount - 2).fadeOut(0);
+            console.log(slideIndex);
+        }
+
+
         slideShow.animate({
             left: (-slideIndex * 100) + "%"
         }, 1000, function () {
 
             $(".slide-caption").eq(slideIndex).fadeIn(1000);
+            
+            
+        if (slideIndex == slideCount - 1) {
+            $(".slide-caption").eq(1).fadeIn(1000);
+            console.log(slideIndex);
+        }
+        if (slideIndex == 0) {
+            $(".slide-caption").eq(slideCount - 2).fadeIn(1000);
+            console.log(slideIndex);
+        }            
+            
             enable = true;
         });
     }
 
     $(".single-slide").each(function (index) {
-        $(this).css("width", slideWidth+1 + "%");
+        $(this).css("width", slideWidth + 1 + "%");
         $(this).css("margin-left", slideWidth * index + "%");
     });
 
